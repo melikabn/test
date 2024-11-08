1cde5597fe90cbfc7a2d9b1c2db60a4af39731c5
melikabn <more.unique1400@gmail.com>
2024-11-08 12:05:13 +0330

    this is first commit in fron developing

---
+++ main.js
@@ -0,0 +1,16 @@
+let score = prompt("enter your score .........")
+if (score >= 17 && score <=20){
+    console.log(`you got A score`)
+}
+else if (score >= 14 && score <17){
+    console.log(`you got B score`) 
+}
+else if (score >= 10 && score <14){
+    console.log(`you got C score`) 
+}
+else if (score >= 0 && score <10){
+    console.log(`you got F score`) 
+}else{
+        console.log(`just digits . between 0 to 20`) 
+   
+}
